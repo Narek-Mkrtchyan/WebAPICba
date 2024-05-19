9a4c08007dcd7c8ca52ecb8e709e5374ddadafc5
Narek Mkrtchyan <mkrtchyann1993@gmail.com>
2024-05-19 18:40:34 +0400

    add comments

--- WebAPICba/Controllers/ExchangeRateController.cs
+++ WebAPICba/Controllers/ExchangeRateController.cs
@@ -1,8 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
-using WebAPICba.Services; 
+using WebAPICba.Services;
 
 namespace WebAPICba.Controllers
 {
@@ -12,24 +9,37 @@ namespace WebAPICba.Controllers
     {
         private readonly ExchangeRateService _exchangeRateService;
 
+        // Constructor to inject ExchangeRateService instance
         public ExchangeRateController(ExchangeRateService exchangeRateService)
         {
             _exchangeRateService = exchangeRateService;
         }
 
+        // Action to fetch and save exchange rates based on provided parameters
         [HttpGet("fetch")]
         public async Task<IActionResult> FetchExchangeRates([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo, [FromQuery] string isoCodes)
         {
+            // Split ISO codes string into an array
             var isoCodeArray = isoCodes.Split(',');
+
+            // Call ExchangeRateService method to fetch and save exchange rates asynchronously
             await _exchangeRateService.FetchAndSaveExchangeRates(dateFrom, dateTo, isoCodeArray);
+
+            // Return OK response with a message indicating success
             return Ok("Exchange rates fetched and saved.");
         }
 
+        // Action to get exchange rates based on provided parameters
         [HttpGet]
         public async Task<IActionResult> GetExchangeRates([FromQuery] DateTime dateFrom, [FromQuery] DateTime dateTo, [FromQuery] string isoCodes)
         {
+            // Split ISO codes string into an array
             var isoCodeArray = isoCodes.Split(',');
+
+            // Call ExchangeRateService method to get exchange rates asynchronously
             var rates = await _exchangeRateService.GetExchangeRates(dateFrom, dateTo, isoCodeArray);
+
+            // Return OK response with the exchange rates retrieved from the service
             return Ok(rates);
         }
     }
--- WebAPICba/Services/ExchangeRateService.cs
+++ WebAPICba/Services/ExchangeRateService.cs
@@ -16,29 +16,35 @@ namespace WebAPICba.Services
         private readonly ExchangeRateContext _context;
         private readonly ConfigurationService _configurationService;
 
+        // Constructor to inject dependencies
         public ExchangeRateService(ExchangeRateContext context, ConfigurationService configurationService)
         {
             _context = context;
             _configurationService = configurationService;
         }
 
+        // Method to fetch exchange rates from the CBA API and save them to the database
         public async Task FetchAndSaveExchangeRates(DateTime startDate, DateTime endDate, string[] isoCodes)
         {
+            // Loop through each date in the specified range
             for (var date = startDate; date <= endDate; date = date.AddDays(1))
             {
+                // Construct SOAP envelope using template retrieved from configuration service
                 var soapTemplate = _configurationService.GetSoapTemplate("ExchangeRatesByDate");
                 var soapEnvelope = string.Format(soapTemplate, date);
 
+                // Send SOAP request to CBA API
                 using var httpClient = new HttpClient();
                 var content = new StringContent(soapEnvelope, Encoding.UTF8, "text/xml");
                 content.Headers.Add("SOAPAction", "http://www.cba.am/ExchangeRatesByDate");
-
                 var response = await httpClient.PostAsync("https://api.cba.am/exchangerates.asmx", content);
                 response.EnsureSuccessStatusCode();
 
+                // Parse response XML
                 var responseContent = await response.Content.ReadAsStringAsync();
                 var xmlDoc = XDocument.Parse(responseContent);
 
+                // Extract exchange rates from XML response
                 var rates = xmlDoc.Descendants(XName.Get("ExchangeRate", "http://www.cba.am/"))
                     .Where(x => isoCodes.Contains(x.Element(XName.Get("ISO", "http://www.cba.am/"))?.Value))
                     .Select(x => new ExchangeRate
@@ -49,10 +55,12 @@ namespace WebAPICba.Services
                     })
                     .ToList();
 
+                // Retrieve existing rates from the database
                 var existingRates = await _context.ExchangeRates
                     .Where(r => r.Date == date && isoCodes.Contains(r.Currency))
                     .ToListAsync();
 
+                // Update or add new rates to the database
                 foreach (var rate in rates)
                 {
                     var existingRate = existingRates.FirstOrDefault(r => r.Currency == rate.Currency);
@@ -67,10 +75,12 @@ namespace WebAPICba.Services
                     }
                 }
 
+                // Save changes to the database
                 await _context.SaveChangesAsync();
             }
         }
 
+        // Method to get exchange rates from the database
         public async Task<List<ExchangeRate>> GetExchangeRates(DateTime startDate, DateTime endDate, string[] isoCodes)
         {
             return await _context.ExchangeRates
